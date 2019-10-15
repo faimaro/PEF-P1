b2f447a00b8550b50e43538f7c6f2702240c1985
faimaro <aimarofran@gmail.com-at-456970202383>
2019-10-15 17:52:28 -0300

    fran

--- Programa1 Original/TEST/Test6/TEST_6.sh
+++ Programa1 Original/TEST/Test6/TEST_6.sh
@@ -1,12 +1,13 @@
 # test -e prueba.dat && echo "file1 exists" || echo "file1 does not exist"
 
 if [ -e $final ]; then
-  result=$(./a)
+    result=$(./a)
+    printf "datos %s\n" $result 
+#   printf "Test %s\n" $result
+#   for r in $result ; do
+#   printf "Test %s\n" $r
 
-  for r in $result ; do
-    printf "Test %s\n" $r
-
-  done
-else
-  printf "Utilizar:\n\t%s NombreArchivo\n" $0
+#   done
+# else
+#   printf "Utilizar:\n\t%s NombreArchivo\n" $0
 fi
