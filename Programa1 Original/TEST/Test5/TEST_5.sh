f09c78032715a637492e9c426b2b233fa8e2aff4
Rodrigo <roodrigosou@gmail.com>
2019-10-15 16:17:33 -0300

    Test de buenas practicas

--- Programa1 Original/TEST/Test5/TEST_5.sh
+++ Programa1 Original/TEST/Test5/TEST_5.sh
@@ -2,14 +2,31 @@
 #
 # Autor: Rodrigo Souza
 #
-
 if [ -e $final ]; then
-  result=$(./prue)
+
+  #Escribir el archivo prueba.dat
+
+  #result=$(./FinalOriginal) #Codigo Original 
+  result=$(./FinalCorregido) #Codigo Corregido
 
   for r in $result ; do
-    printf "Test %s\n" $r
 
+    echo $r
+    case $r in 
+      "837.3") 
+        echo Codigo 1, paso
+        ;;
+      "2044.03")  
+        echo Codigo 2, paso
+        ;;
+      "3606.12") 
+        echo Codigo 3, paso
+        ;;
+      *)
+        echo No paso
+    esac
   done
+
 else
   printf "Utilizar:\n\t%s NombreArchivo\n" $0
 fi
